48fcb88a9b4adb092b1899b55dc5b6318dbaeee3
Alexandre Roman <alexandre.roman@broadcom.com>
2023-12-06 16:29:46 +0100

    Fix Spring MVC issue with GraalVM

--- src/main/java/com/vmware/tanzu/demos/springflix/trailers/impl/WebConfig.java
+++ src/main/java/com/vmware/tanzu/demos/springflix/trailers/impl/WebConfig.java
@@ -17,12 +17,14 @@
 package com.vmware.tanzu.demos.springflix.trailers.impl;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportRuntimeHints;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration(proxyBeanMethods = false)
 @EnableWebMvc
+@ImportRuntimeHints(WebNativeConfig.class)
 class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
